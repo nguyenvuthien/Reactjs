7b37dda4a173b52fc7666bd55cf8d4a31cedcd93
Nguyen Vu Thien <thien180102@gmail.com>
2023-04-23 17:22:51 +0700

    video 63 reactjs

--- src/containers/System/Admin/TableUserManage.js
+++ src/containers/System/Admin/TableUserManage.js
@@ -32,8 +32,6 @@ class TableUserManage extends Component {
         this.props.handleEditUserFromParentKey(user)
     }
     render() {
-        console.log('check all users: ', this.props.listUsers)
-        console.log('check state: ', this.state.usersRedux)
         let arrUsers = this.state.usersRedux;
         return (
             <table id='TableUserManage'>
