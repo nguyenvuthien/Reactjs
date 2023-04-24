9c98dc387692b19cb73e3e0428c2186d5d53f8d1
Nguyen Vu Thien <thien180102@gmail.com>
2023-04-24 17:26:15 +0700

    video 69 reactjs

--- src/containers/HomePage/Section/OutStandingDoctor.js
+++ src/containers/HomePage/Section/OutStandingDoctor.js
@@ -4,6 +4,7 @@ import { FormattedMessage } from 'react-intl';
 import Slider from "react-slick";
 import * as actions from '../../../store/actions';
 import { LANGUAGES } from '../../../utils';
+import { withRouter } from 'react-router';
 
 class OutStandingDoctor extends Component {
 
@@ -22,6 +23,10 @@ class OutStandingDoctor extends Component {
     }
     componentDidMount() {
         this.props.loadTopDoctors();
+    }
+    handleViewDetailDoctor = (doctor) => {
+        console.log('view infor: ', doctor)
+        this.props.history.push(`/detail-doctor/${doctor.id}`)
     }
     render() {
         let arrDoctors = this.state.arrDoctors;
@@ -45,7 +50,7 @@ class OutStandingDoctor extends Component {
                                     let nameVi = `${item.positionData.valueVi}, ${item.lastName} ${item.firstName}`;
                                     let nameEn = `${item.positionData.valueEn}, ${item.firstName} ${item.lastName}`;
                                     return (
-                                        <div className='section-customize' key={index}>
+                                        <div className='section-customize' key={index} onClick={() => this.handleViewDetailDoctor(item)}>
                                             <div className='customize-border'>
                                                 <div className='outer-bg'>
                                                     <div className='bg-image section-outstanding-doctor'
@@ -83,4 +88,4 @@ const mapDispatchToProps = dispatch => {
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(OutStandingDoctor);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(OutStandingDoctor));
