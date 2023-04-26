ad2b7ab908aedcb133b9caece97903cdf03c21cb
Nguyen Vu Thien <thien180102@gmail.com>
2023-04-26 16:16:37 +0700

    video 80 reactjs

--- src/containers/Patient/Doctor/DetailDoctor.js
+++ src/containers/Patient/Doctor/DetailDoctor.js
@@ -5,6 +5,7 @@ import './DetailDoctor.scss'
 import { getDetailInforDoctor } from '../../../services/userService'
 import { LANGUAGES } from '../../../utils';
 import DoctorSchedule from './DoctorSchedule';
+import DoctorExtraInfor from './DoctorExtraInfor';
 class DetailDoctor extends Component {
 
     constructor(props) {
@@ -70,7 +71,7 @@ class DetailDoctor extends Component {
                             />
                         </div>
                         <div className='content-right'>
-
+                            <DoctorExtraInfor />
                         </div>
                     </div>
                     <div className='detail-infor-doctor'>
