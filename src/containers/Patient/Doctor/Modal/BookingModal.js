b095d4d1bae8cd2ff1dfe4e6718ebabf8605472b
Nguyen Vu Thien <thien180102@gmail.com>
2023-04-26 21:58:07 +0700

    video 84 reactjs

--- src/containers/Patient/Doctor/Modal/BookingModal.js
+++ src/containers/Patient/Doctor/Modal/BookingModal.js
@@ -52,6 +52,8 @@ class BookingModal extends Component {
                         <div className='doctor-infor'>
                             <ProfileDoctor
                                 doctorId={doctorId}
+                                isShowDescriptionDoctor={false}
+                                dataTime={dataTime}
                             />
                         </div>
                         <div className='price'>
